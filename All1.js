7d200e5b095675ec3a9cc7c07b6dacdc232f239c
JUGKIT0116 <127175607+JUGKIT0116@users.noreply.github.com>
2023-06-08 18:54:05 +0800

    Add files via upload

---
+++ All1.js
@@ -0,0 +1,652 @@
+
+//ZYJ
+document.getElementById('zhezhaoZYJ').style.display="none";
+
+function dianwoZYJ(){
+  document.getElementById('zhezhaoZYJ').style.display="";
+}
+
+function hidderZYJ(){
+
+  document.getElementById('zhezhaoZYJ').style.display="none";
+
+}
+
+//SJ
+document.getElementById('zhezhaoSJ').style.display="none";
+
+function dianwoSJ(){
+  document.getElementById('zhezhaoSJ').style.display="";
+}
+
+function hidderSJ(){
+
+  document.getElementById('zhezhaoSJ').style.display="none";
+
+}
+
+//LYC
+document.getElementById('zhezhaoLYC').style.display="none";
+
+function dianwoLYC(){
+  document.getElementById('zhezhaoLYC').style.display="";
+}
+
+function hidderLYC(){
+
+  document.getElementById('zhezhaoLYC').style.display="none";
+
+}
+
+//XL
+document.getElementById('zhezhaoXL').style.display="none";
+
+function dianwoXL(){
+  document.getElementById('zhezhaoXL').style.display="";
+}
+
+function hidderXL(){
+
+  document.getElementById('zhezhaoXL').style.display="none";
+
+}
+
+//YYN
+document.getElementById('zhezhaoYYN').style.display="none";
+
+function dianwoYYN(){
+  document.getElementById('zhezhaoYYN').style.display="";
+}
+
+function hidderYYN(){
+
+  document.getElementById('zhezhaoYYN').style.display="none";
+
+}
+
+//ZMM
+document.getElementById('zhezhaoZMM').style.display="none";
+
+function dianwoZMM(){
+  document.getElementById('zhezhaoZMM').style.display="";
+}
+
+function hidderZMM(){
+
+  document.getElementById('zhezhaoZMM').style.display="none";
+
+}
+
+//WZ
+document.getElementById('zhezhaoWZ').style.display="none";
+
+function dianwoWZ(){
+  document.getElementById('zhezhaoWZ').style.display="";
+}
+
+function hidderWZ(){
+
+  document.getElementById('zhezhaoWZ').style.display="none";
+
+}
+
+//ZYR
+document.getElementById('zhezhaoZYR').style.display="none";
+
+function dianwoZYR(){
+  document.getElementById('zhezhaoZYR').style.display="";
+}
+
+function hidderZYR(){
+
+  document.getElementById('zhezhaoZYR').style.display="none";
+
+}
+
+//TYR
+document.getElementById('zhezhaoTYR').style.display="none";
+
+function dianwoTYR(){
+  document.getElementById('zhezhaoTYR').style.display="";
+}
+
+function hidderTYR(){
+
+  document.getElementById('zhezhaoTYR').style.display="none";
+
+}
+
+//ZMY
+document.getElementById('zhezhaoZMY').style.display="none";
+
+function dianwoZMY(){
+  document.getElementById('zhezhaoZMY').style.display="";
+}
+
+function hidderZMY(){
+
+  document.getElementById('zhezhaoZMY').style.display="none";
+
+}
+
+//HZY
+document.getElementById('zhezhaoHZY').style.display="none";
+
+function dianwoHZY(){
+  document.getElementById('zhezhaoHZY').style.display="";
+}
+
+function hidderHZY(){
+
+  document.getElementById('zhezhaoHZY').style.display="none";
+
+}
+
+//ZMY
+document.getElementById('zhezhaoZMY').style.display="none";
+
+function dianwoZMY(){
+  document.getElementById('zhezhaoZMY').style.display="";
+}
+
+function hidderZMY(){
+
+  document.getElementById('zhezhaoZMY').style.display="none";
+
+}
+
+//ZZY
+document.getElementById('zhezhaoZZY').style.display="none";
+
+function dianwoZZY(){
+  document.getElementById('zhezhaoZZY').style.display="";
+}
+
+function hidderZZY(){
+
+  document.getElementById('zhezhaoZZY').style.display="none";
+
+}
+
+//YSY
+document.getElementById('zhezhaoYSY').style.display="none";
+
+function dianwoYSY(){
+  document.getElementById('zhezhaoYSY').style.display="";
+}
+
+function hidderYSY(){
+
+  document.getElementById('zhezhaoYSY').style.display="none";
+
+}
+
+//HXY
+document.getElementById('zhezhaoHXY').style.display="none";
+
+function dianwoHXY(){
+  document.getElementById('zhezhaoHXY').style.display="";
+}
+
+function hidderHXY(){
+
+  document.getElementById('zhezhaoHXY').style.display="none";
+
+}
+
+//LJQ
+document.getElementById('zhezhaoLJQ').style.display="none";
+
+function dianwoLJQ(){
+  document.getElementById('zhezhaoLJQ').style.display="";
+}
+
+function hidderLJQ(){
+
+  document.getElementById('zhezhaoLJQ').style.display="none";
+
+}
+
+//PSQ
+document.getElementById('zhezhaoPSQ').style.display="none";
+
+function dianwoPSQ(){
+  document.getElementById('zhezhaoPSQ').style.display="";
+}
+
+function hidderPSQ(){
+
+  document.getElementById('zhezhaoPSQ').style.display="none";
+
+}
+
+//SHR
+document.getElementById('zhezhaoSHR').style.display="none";
+
+function dianwoSHR(){
+  document.getElementById('zhezhaoSHR').style.display="";
+}
+
+function hidderSHR(){
+
+  document.getElementById('zhezhaoSHR').style.display="none";
+
+}
+
+//YYM
+document.getElementById('zhezhaoYYM').style.display="none";
+
+function dianwoYYM(){
+  document.getElementById('zhezhaoYYM').style.display="";
+}
+
+function hidderYYM(){
+
+  document.getElementById('zhezhaoYYM').style.display="none";
+
+}
+
+//WJY
+document.getElementById('zhezhaoWJY').style.display="none";
+
+function dianwoWJY(){
+  document.getElementById('zhezhaoWJY').style.display="";
+}
+
+function hidderWJY(){
+
+  document.getElementById('zhezhaoWJY').style.display="none";
+
+}
+
+//YJ
+document.getElementById('zhezhaoYJ').style.display="none";
+
+function dianwoYJ(){
+  document.getElementById('zhezhaoYJ').style.display="";
+}
+
+function hidderYJ(){
+
+  document.getElementById('zhezhaoYJ').style.display="none";
+
+}
+
+//HPY
+document.getElementById('zhezhaoHPY').style.display="none";
+
+function dianwoHPY(){
+  document.getElementById('zhezhaoHPY').style.display="";
+}
+
+function hidderHPY(){
+
+  document.getElementById('zhezhaoHPY').style.display="none";
+
+}
+
+//LYX
+document.getElementById('zhezhaoLYX').style.display="none";
+
+function dianwoLYX(){
+  document.getElementById('zhezhaoLYX').style.display="";
+}
+
+function hidderLYX(){
+
+  document.getElementById('zhezhaoLYX').style.display="none";
+
+}
+
+//WLR
+document.getElementById('zhezhaoWLR').style.display="none";
+
+function dianwoWLR(){
+  document.getElementById('zhezhaoWLR').style.display="";
+}
+
+function hidderWLR(){
+
+  document.getElementById('zhezhaoWLR').style.display="none";
+
+}
+
+//QXY
+document.getElementById('zhezhaoQXY').style.display="none";
+
+function dianwoQXY(){
+  document.getElementById('zhezhaoQXY').style.display="";
+}
+
+function hidderQXY(){
+
+  document.getElementById('zhezhaoQXY').style.display="none";
+
+}
+
+//CXR
+document.getElementById('zhezhaoCXR').style.display="none";
+
+function dianwoCXR(){
+  document.getElementById('zhezhaoCXR').style.display="";
+}
+
+function hidderCXR(){
+
+  document.getElementById('zhezhaoCXR').style.display="none";
+
+}
+
+//ZXY
+document.getElementById('zhezhaoZXY').style.display="none";
+
+function dianwoZXY(){
+  document.getElementById('zhezhaoZXY').style.display="";
+}
+
+function hidderZXY(){
+
+  document.getElementById('zhezhaoZXY').style.display="none";
+
+}
+
+//TK
+document.getElementById('zhezhaoTK').style.display="none";
+
+function dianwoTK(){
+  document.getElementById('zhezhaoTK').style.display="";
+}
+
+function hidderTK(){
+
+  document.getElementById('zhezhaoTK').style.display="none";
+
+}
+
+//QLY
+document.getElementById('zhezhaoQLY').style.display="none";
+
+function dianwoQLY(){
+  document.getElementById('zhezhaoQLY').style.display="";
+}
+
+function hidderQLY(){
+
+  document.getElementById('zhezhaoQLY').style.display="none";
+
+}
+
+//LJY
+document.getElementById('zhezhaoLJY').style.display="none";
+
+function dianwoLJY(){
+  document.getElementById('zhezhaoLJY').style.display="";
+}
+
+function hidderLJY(){
+
+  document.getElementById('zhezhaoLJY').style.display="none";
+
+}
+
+//BQL
+document.getElementById('zhezhaoBQL').style.display="none";
+
+function dianwoBQL(){
+  document.getElementById('zhezhaoBQL').style.display="";
+}
+
+function hidderBQL(){
+
+  document.getElementById('zhezhaoBQL').style.display="none";
+
+}
+
+//MYY
+document.getElementById('zhezhaoMYY').style.display="none";
+
+function dianwoMYY(){
+  document.getElementById('zhezhaoMYY').style.display="";
+}
+
+function hidderMYY(){
+
+  document.getElementById('zhezhaoMYY').style.display="none";
+
+}
+
+//TT
+document.getElementById('zhezhaoTT').style.display="none";
+
+function dianwoTT(){
+  document.getElementById('zhezhaoTT').style.display="";
+}
+
+function hidderTT(){
+
+  document.getElementById('zhezhaoTT').style.display="none";
+
+}
+
+//LWX
+document.getElementById('zhezhaoLWX').style.display="none";
+
+function dianwoLWX(){
+  document.getElementById('zhezhaoLWX').style.display="";
+}
+
+function hidderLWX(){
+
+  document.getElementById('zhezhaoLWX').style.display="none";
+
+}
+
+//ZXY
+document.getElementById('zhezhaoZXY').style.display="none";
+
+function dianwoZXY(){
+  document.getElementById('zhezhaoZXY').style.display="";
+}
+
+function hidderZXY(){
+
+  document.getElementById('zhezhaoZXY').style.display="none";
+
+}
+
+//XCN
+document.getElementById('zhezhaoXCN').style.display="none";
+
+function dianwoXCN(){
+  document.getElementById('zhezhaoXCN').style.display="";
+}
+
+function hidderXCN(){
+
+  document.getElementById('zhezhaoXCN').style.display="none";
+
+}
+
+//LWJ
+document.getElementById('zhezhaoLWJ').style.display="none";
+
+function dianwoLWJ(){
+  document.getElementById('zhezhaoLWJ').style.display="";
+}
+
+function hidderLWJ(){
+
+  document.getElementById('zhezhaoLWJ').style.display="none";
+
+}
+
+//LWN
+document.getElementById('zhezhaoLWN').style.display="none";
+
+function dianwoLWN(){
+  document.getElementById('zhezhaoLWN').style.display="";
+}
+
+function hidderLWN(){
+
+  document.getElementById('zhezhaoLWN').style.display="none";
+
+}
+
+//WYT
+document.getElementById('zhezhaoWYT').style.display="none";
+
+function dianwoWYT(){
+  document.getElementById('zhezhaoWYT').style.display="";
+}
+
+function hidderWYT(){
+
+  document.getElementById('zhezhaoWYT').style.display="none";
+
+}
+
+//XJW
+document.getElementById('zhezhaoXJW').style.display="none";
+
+function dianwoXJW(){
+  document.getElementById('zhezhaoXJW').style.display="";
+}
+
+function hidderXJW(){
+
+  document.getElementById('zhezhaoXJW').style.display="none";
+
+}
+
+
+//MYL
+document.getElementById('zhezhaoMYL').style.display="none";
+
+function dianwoMYL(){
+  document.getElementById('zhezhaoMYL').style.display="";
+}
+
+function hidderMYL(){
+
+  document.getElementById('zhezhaoMYL').style.display="none";
+
+}
+
+
+//MXY
+document.getElementById('zhezhaoMXY').style.display="none";
+
+function dianwoMXY(){
+  document.getElementById('zhezhaoMXY').style.display="";
+}
+
+function hidderMXY(){
+
+  document.getElementById('zhezhaoMXY').style.display="none";
+
+}
+
+//PSY
+document.getElementById('zhezhaoPSY').style.display="none";
+
+function dianwoPSY(){
+  document.getElementById('zhezhaoPSY').style.display="";
+}
+
+function hidderPSY(){
+
+  document.getElementById('zhezhaoPSY').style.display="none";
+
+}
+
+//ZYN
+document.getElementById('zhezhaoZYN').style.display="none";
+
+function dianwoZYN(){
+  document.getElementById('zhezhaoZYN').style.display="";
+}
+
+function hidderZYN(){
+
+  document.getElementById('zhezhaoZYN').style.display="none";
+
+}
+
+//LJX
+document.getElementById('zhezhaoLJX').style.display="none";
+
+function dianwoLJX(){
+  document.getElementById('zhezhaoLJX').style.display="";
+}
+
+function hidderLJX(){
+
+  document.getElementById('zhezhaoLJX').style.display="none";
+
+}
+
+//ZYT
+document.getElementById('zhezhaoZYT').style.display="none";
+
+function dianwoZYT(){
+  document.getElementById('zhezhaoZYT').style.display="";
+}
+
+function hidderZYT(){
+
+  document.getElementById('zhezhaoZYT').style.display="none";
+
+}
+
+//YXY
+document.getElementById('zhezhaoYXY').style.display="none";
+
+function dianwoYXY(){
+  document.getElementById('zhezhaoYXY').style.display="";
+}
+
+function hidderYXY(){
+
+  document.getElementById('zhezhaoYXY').style.display="none";
+
+}
+
+//CYQ
+document.getElementById('zhezhaoCYQ').style.display="none";
+
+function dianwoCYQ(){
+  document.getElementById('zhezhaoCYQ').style.display="";
+}
+
+function hidderCYQ(){
+
+  document.getElementById('zhezhaoCYQ').style.display="none";
+
+}
+
+//GYX
+document.getElementById('zhezhaoGYX').style.display="none";
+
+function dianwoGYX(){
+  document.getElementById('zhezhaoGYX').style.display="";
+}
+
+function hidderGYX(){
+
+  document.getElementById('zhezhaoGYX').style.display="none";
+
+}
+
+//YWWY
+document.getElementById('zhezhaoYWWY').style.display="none";
+
+function dianwoYWWY(){
+  document.getElementById('zhezhaoYWWY').style.display="";
+}
+
+function hidderYWWY(){
+
+  document.getElementById('zhezhaoYWWY').style.display="none";
+
+}
